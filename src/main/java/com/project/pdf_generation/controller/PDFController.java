9d9db906e0d2e466185ef48fb4da3f8efb5222ee
shrikrishna <shrikrishnaprasad44@gmail.com>
2024-10-19 19:24:39 +0530

    fix: pdf generation

--- src/main/java/com/project/pdf_generation/controller/PDFController.java
+++ src/main/java/com/project/pdf_generation/controller/PDFController.java
@@ -1,19 +1,14 @@
 package com.project.pdf_generation.controller;
 
-import com.itextpdf.text.DocumentException;
 import com.project.pdf_generation.model.InvoiceDetails;
 import com.project.pdf_generation.service.PDFService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 
 @RestController
 @RequestMapping("/pdf")
