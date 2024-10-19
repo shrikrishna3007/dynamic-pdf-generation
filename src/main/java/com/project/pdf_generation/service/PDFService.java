1afd7d88cdaa2c5872799fa75133fc1c49eb7c8a
shrikrishna <shrikrishnaprasad44@gmail.com>
2024-10-19 18:52:58 +0530

    feature: pdf generation using itext

--- src/main/java/com/project/pdf_generation/service/PDFService.java
+++ src/main/java/com/project/pdf_generation/service/PDFService.java
@@ -48,35 +48,65 @@ public class PDFService {
         document.add(new Paragraph(" "));
         PdfPTable table = new PdfPTable(4);
         table.setWidthPercentage(100);
+        table.setPaddingTop(10);
 
         PdfPCell cell1 = new PdfPCell();
         cell1.setRowspan(6);
         cell1.setColspan(2);
+        cell1.setPadding(10);
         Paragraph paragraph1 = new Paragraph();
-        paragraph1.add(new Paragraph("Seller: "));
-        paragraph1.add(new Paragraph(invoiceDetails.getSeller()));
-        paragraph1.add(new Paragraph("GSTIN : "+invoiceDetails.getSellerGSTIN()));
-        paragraph1.add(new Paragraph("Address: "+invoiceDetails.getSellerAddress()));
+        paragraph1.add("Seller: \n");
+        paragraph1.add("Seller: "+invoiceDetails.getSeller()+"\n");
+        paragraph1.add("SellerGSTIN: "+invoiceDetails.getSellerGSTIN()+"\n");
+        paragraph1.add("Seller Address: "+invoiceDetails.getSellerAddress()+"\n");
         cell1.setPhrase(paragraph1);
         table.addCell(cell1);
 
         PdfPCell cell2=new PdfPCell();
         cell2.setRowspan(6);
         cell2.setColspan(2);
+        cell2.setPadding(10);
         Paragraph paragraph2 = new Paragraph();
-        paragraph2.add(new Paragraph("Buyer: "));
-        paragraph2.add(new Paragraph(invoiceDetails.getBuyer()));
-        paragraph2.add(new Paragraph("GSTIN : "+invoiceDetails.getBuyerGSTIN()));
-        paragraph2.add(new Paragraph("Address: "+invoiceDetails.getBuyerAddress()));
+        paragraph2.add("Buyer: \n");
+        paragraph2.add("Buyer"+invoiceDetails.getBuyer()+"\n");
+        paragraph2.add("BuyerGSTIN: "+invoiceDetails.getBuyerGSTIN()+"\n");
+        paragraph2.add("Buyer Address: "+invoiceDetails.getBuyerAddress()+"\n");
         cell2.setPhrase(paragraph2);
         table.addCell(cell2);
 
-        table.addCell("Item");
-        table.addCell("Quantity");
-        table.addCell("Rate");
-        table.addCell("Amount");
+        PdfPCell itemName=new PdfPCell(new Paragraph("Item"));
+        itemName.setPadding(10f);
+        table.addCell(itemName);
 
-        table.addCell(invoiceDetails.getItems().);
+        PdfPCell itemQuantity=new PdfPCell(new Paragraph("Quantity"));
+        itemQuantity.setPadding(10f);
+        table.addCell(itemQuantity);
+
+        PdfPCell itemRate=new PdfPCell(new Paragraph("Rate"));
+        itemRate.setPadding(10f);
+        table.addCell(itemRate);
+
+        PdfPCell itemAmount=new PdfPCell(new Paragraph("Amount"));
+        itemAmount.setPadding(10f);
+        table.addCell(itemAmount);
+
+        for(InvoiceDetails.Items item: invoiceDetails.getItems()){
+            PdfPCell itemNameCell=new PdfPCell(new Paragraph(item.getName()));
+            itemNameCell.setPadding(10f);
+            table.addCell(itemNameCell);
+
+            PdfPCell itemQuantityCell=new PdfPCell(new Paragraph(item.getQuantity()));
+            itemQuantityCell.setPadding(10f);
+            table.addCell(itemQuantityCell);
+
+            PdfPCell itemRateCell=new PdfPCell(new Paragraph(String.valueOf(item.getRate())));
+            itemRateCell.setPadding(10f);
+            table.addCell(itemRateCell);
+
+            PdfPCell itemAmountCell=new PdfPCell(new Paragraph(String.valueOf(item.getAmount())));
+            itemAmountCell.setPadding(10f);
+            table.addCell(itemAmountCell);
+        }
         document.add(table);
         document.close();
     }
@@ -84,6 +114,6 @@ public class PDFService {
     private String generateHash(InvoiceDetails invoiceDetails) throws NoSuchAlgorithmException {
         MessageDigest messageDigest=MessageDigest.getInstance("SHA-256");
         byte[] hash=messageDigest.digest(invoiceDetails.toString().getBytes(StandardCharsets.UTF_8));
-        return Base64.getEncoder().encodeToString(hash);
+        return Base64.getUrlEncoder().encodeToString(hash);
     }
 }
